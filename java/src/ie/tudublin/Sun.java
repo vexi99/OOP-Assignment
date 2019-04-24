d0892ff772aebcc22f56908504ae88c6c3a21d83
Vexi99 <c17448206@mydit.ie>
2019-04-24 19:10:51 +0100

    unfinished changes

--- java/src/ie/tudublin/Sun.java
+++ java/src/ie/tudublin/Sun.java
@@ -1,6 +1,9 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+import java.util.ArrayList;
 
 public class Sun extends PApplet
 {
--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -1,6 +1,9 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+import java.util.ArrayList;
 
 public class UI extends PApplet
 {
@@ -15,17 +18,41 @@ public class UI extends PApplet
 
     public void setup()
     {
+        readFile();
+        printFile();
         //passing variables to classes
         sb = new StarBackground(this, 800, 800);
         sun = new Sun(this, 800, 800);
-        planets = new Planets(this, 800, 800);
+        //planets = new Planets(this,row);
+        //read from csv file
+        
     }
 
     public void draw()
     {
         sb.render();
         sun.render();
-        planets.render();
+        //planets.render();
     }
+
+    public void readFile()
+    {
+        Table table = loadTable("PlanetData.csv", "header");
+
+        for (TableRow row : table.rows()) {
+            Planets planets = new Planets(this,row);
+            planetList.add(planets);
+        }
+    }
+
+    public void printFile()
+    {
+        for (Planets planets: planetList)
+        {
+            System.out.println(planetList);
+        }
+    }
+
+    private ArrayList<Planets> planetList = new ArrayList<Planets>();
 }
 
