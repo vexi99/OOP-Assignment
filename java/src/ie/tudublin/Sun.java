dd7a7b8074c86c3727459bcb46af175e8838fafc
Vexi99 <c17448206@mydit.ie>
2019-04-24 16:04:24 +0100

    Sun created

--- java/src/ie/tudublin/Sun.java
+++ java/src/ie/tudublin/Sun.java
@@ -4,8 +4,30 @@ import processing.core.PApplet;
 
 public class Sun extends PApplet
 {
-    /*
-        draw sun
-        
-    */
+    UI ui;
+    private float scWidth;
+    private float scHeight;
+
+    public Sun(UI ui, float scWidth, float scHeight)
+    {
+        //passing variables from UI.java
+        this.ui = ui;
+        this.scWidth = scWidth;
+        this.scHeight = scHeight;
+    }
+
+    public void render()
+    {
+        //start of drawing of sun
+        ui.fill(255, 0, 16);
+        ui.noStroke();
+        ui.circle(400,400,50);
+    }
 }
+
+
+/*
+/ "Sun" class which handles the drawing of the sun "TRAPPIST-1".
+/ Which is at the center of the "Trappist-1" solar system which 
+/ my project is based on.
+*/
--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -5,6 +5,8 @@ import processing.core.PApplet;
 public class UI extends PApplet
 {
     StarBackground sb;
+    Sun sun;
+
     
     public void settings()
     {
@@ -13,7 +15,9 @@ public class UI extends PApplet
 
     public void setup()
     {
+        //passing variables to classes
         sb = new StarBackground(this, 800, 800);
+        sun = new Sun(this, 800, 800);
     }
 
 
@@ -21,6 +25,7 @@ public class UI extends PApplet
     {
         //background(0);
         sb.render();
+        sun.render();
         
     }
 }
