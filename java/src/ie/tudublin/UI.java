0c5f8de87097284f7d1b6bf0081f890b613c2e36
Vexi99 <c17448206@mydit.ie>
2019-04-28 11:06:12 +0100

    working on planet movement

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -4,12 +4,14 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 import java.util.ArrayList;
+import processing.core.PVector;
 
 public class UI extends PApplet
 {
     StarBackground sb;
     Sun sun;
     Planets planets;
+    Radar radar;
 
     public void settings()
     {
@@ -22,7 +24,8 @@ public class UI extends PApplet
         //passing variables to classes
         sb = new StarBackground(this, 800, 800);
         sun = new Sun(this, 800, 800);
-        planets = new Planets(this);
+        planets = new Planets(this, 1, 400, 400, 100);
+        radar = new Radar(this, 1, 400,400,100);
         //read from csv file
         
     }
@@ -32,6 +35,9 @@ public class UI extends PApplet
         sb.render();
         sun.render();
         planets.render();
+
+        radar.update();
+        radar.render();
     }
 
     /*public void readFile()
