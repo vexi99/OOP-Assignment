6db0272948825db2a5233092ef7ef13ff8e79c13
Vexi99 <c17448206@mydit.ie>
2019-04-28 13:10:01 +0100

    moving planet orbits fixed and completed

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -11,7 +11,12 @@ public class UI extends PApplet
     StarBackground sb;
     Sun sun;
     Planets planets;
-    Planets2 planets2;
+    Planets planets2;
+    Planets planets3;
+    Planets planets4;
+    Planets planets5;
+    Planets planets6;
+    Planets planets7;
 
     public void settings()
     {
@@ -24,8 +29,13 @@ public class UI extends PApplet
         //passing variables to classes
         sb = new StarBackground(this, 800, 800);
         sun = new Sun(this, 800, 800);
-        planets = new Planets(this, 1, 400, 400, 55);
-        planets2 = new Planets2(this, 1, 400, 400, 110);
+        planets = new Planets(this, 4, 400, 400, 55);
+        planets2 = new Planets(this, 5, 400, 400, 110);
+        planets3 = new Planets(this, 6, 400, 400, 165);
+        planets4 = new Planets(this, 7, 400, 400, 220);
+        planets5 = new Planets(this, 8, 400, 400, 275);
+        planets6 = new Planets(this, 9, 400, 400, 330);
+        planets7 = new Planets(this, 10, 400, 400, 385);
         //read from csv file
         
     }
@@ -38,6 +48,16 @@ public class UI extends PApplet
         planets.update();
         planets2.render();
         planets2.update();
+        planets3.render();
+        planets3.update();
+        planets4.render();
+        planets4.update();
+        planets5.render();
+        planets5.update();
+        planets6.render();
+        planets6.update();
+        planets7.render();
+        planets7.update();
     }
 
     /*public void readFile()
