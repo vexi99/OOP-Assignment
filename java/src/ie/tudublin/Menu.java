2377efc12c3ac6941249a35e0c1a930c67ed1a4b
Vexi99 <c17448206@mydit.ie>
2019-04-29 19:43:27 +0100

    menu fix

--- java/src/ie/tudublin/Menu.java
+++ java/src/ie/tudublin/Menu.java
@@ -23,9 +23,15 @@ public class Menu extends PApplet
         ui.background(0);
         ui.fill(255);
         ui.textAlign(CENTER);
-        ui.textSize(40);
+        ui.textSize(37);
         ui.text("Hello this is help the menu", scHeight/2,100);
         ui.text("Left Arrow Key Slows Down The Planets", scHeight/2, 200);
         ui.text("Right Arrow Key Speeds Up The Planets", scHeight/2, 300);
+        ui.text("Space Bar Normalises Planet's Speed", scHeight/2, 400);
+        ui.text("-------------------------------------------------------------------------", scHeight/2, 450);
+        ui.text("TRAPPIST-1 is a miniscule solar system", scHeight/2, 500);
+        ui.text("In which the planets orbit a dwarf star", scHeight/2, 600);
+        ui.text("3 planets are in the habital zone for humans", scHeight/2, 700);
+
     }
 }
