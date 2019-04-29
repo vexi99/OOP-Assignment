67ffa5da7c44fcead7f383ec5caaf1731826bad3
Vexi99 <c17448206@mydit.ie>
2019-04-29 19:14:42 +0100

    planet names

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -18,6 +18,9 @@ public class UI extends PApplet
     Planets planets5;
     Planets planets6;
     Planets planets7;
+    TableRow row;
+    Table table;
+    String namee;
 
     boolean[] keys = new boolean[1024];
 
@@ -53,13 +56,13 @@ public class UI extends PApplet
 
         menu = new Menu(this);
 
-        planets1 = new Planets(this, 4, 400, 400, 55, 244, 151, 12);
-        planets2 = new Planets(this, 5, 400, 400, 110, 216, 158, 71);
-        planets3 = new Planets(this, 6, 400, 400, 165, 183, 145, 82);
-        planets4 = new Planets(this, 7, 400, 400, 220, 219, 188, 142);
-        planets5 = new Planets(this, 8, 400, 400, 275, 174, 193, 139);
-        planets6 = new Planets(this, 9, 400, 400, 330, 147, 160, 122);
-        planets7 = new Planets(this, 10, 400, 400, 385, 213, 219, 201);
+        planets1 = new Planets(this, 4, 400, 400, 55, 244, 151, 12, row);
+        planets2 = new Planets(this, 5, 400, 400, 110, 216, 158, 71, row);
+        planets3 = new Planets(this, 6, 400, 400, 165, 183, 145, 82, row);
+        planets4 = new Planets(this, 7, 400, 400, 220, 219, 188, 142, row);
+        planets5 = new Planets(this, 8, 400, 400, 275, 174, 193, 139, row);
+        planets6 = new Planets(this, 9, 400, 400, 330, 147, 160, 122, row);
+        planets7 = new Planets(this, 10, 400, 400, 385, 213, 219, 201, row);
     }
 
     public void draw()
@@ -88,6 +91,32 @@ public class UI extends PApplet
 
         planets7.render();
         planets7.update();
+
+        //setting Name of planets, encountered errors in doing so, this was was the only way which worked
+        row = table.getRow(0);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        row = table.getRow(1);
+        planets2.setName(row.getString("Name"));
+        planets2.getName();
+        row = table.getRow(2);
+        planets3.setName(row.getString("Name"));
+        planets3.getName();
+        row = table.getRow(3);
+        planets4.setName(row.getString("Name"));
+        planets4.getName();
+        row = table.getRow(4);
+        planets5.setName(row.getString("Name"));
+        planets5.getName();
+        row = table.getRow(5);
+        planets6.setName(row.getString("Name"));
+        planets6.getName();
+        row = table.getRow(6);
+        planets7.setName(row.getString("Name"));
+        planets7.getName();
+        
+        
+        
         
         if (checkKey(LEFT))
         {
@@ -134,15 +163,47 @@ public class UI extends PApplet
     public void readFile()
     {
         //read in file data
-        Table table = loadTable("PlanetData.csv", "header");
+        table = loadTable("PlanetData.csv", "header");
         //print file data to output
+       
         for (int i = 0; i<table.getRowCount(); i++) 
         {
-            TableRow row = table.getRow(i);
+            row = table.getRow(i);
             System.out.println(row.getString("Name"));
             System.out.println(row.getFloat("Planet Mass"));
-            
         }
+        
+        /*
+        row = table.getRow(1);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        System.out.println(namee);
+        row = table.getRow(2);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        System.out.println(namee);
+        row = table.getRow(3);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        System.out.println(namee);
+        row = table.getRow(4);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        System.out.println(namee);
+        row = table.getRow(5);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        System.out.println(namee);
+        row = table.getRow(6);
+        planets1.setName(row.getString("Name"));
+        planets1.getName();
+        System.out.println(namee);
+        */
+        //row = table.getRow(0);
+        //planets1.setName(row.getString("Name"));
+        
+        
+        
     }
     
 
